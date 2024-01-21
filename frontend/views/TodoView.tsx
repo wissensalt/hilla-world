23d77a00a66377901d959ce05edeb2a574f60931
wissensalt <wissensalt@gmail.com>
2024-01-21 19:36:05 +0700

    fix: better UI

--- frontend/views/TodoView.tsx
+++ frontend/views/TodoView.tsx
@@ -5,25 +5,26 @@ import {TextField} from "@hilla/react-components/TextField";
 import {Button} from "@hilla/react-components/Button";
 import {Checkbox} from "@hilla/react-components/Checkbox";
 import TodoResponse from "Frontend/generated/com/example/application/dto/TodoResponse";
+import {VerticalLayout} from "@hilla/react-components/VerticalLayout";
+import {HorizontalLayout} from "@hilla/react-components/HorizontalLayout";
 
 export default function TodoView() {
   const [todoResponse, setTodoResponse] = useState<TodoResponse>(
       {completed: [], unCompleted: []}
   );
   const [task, setTask] = useState('');
-  const [message, setMessage] = useState('');
   useEffect(() => {
     TodoEndpoint.findAll().then(setTodoResponse)
   }, []);
 
   async function addTodo() {
-    console.log("ADDED TASK ", task)
     const savedTodo = await TodoEndpoint.add(task)
     if (savedTodo) {
-      todoResponse.unCompleted = [...todoResponse.unCompleted as [], savedTodo]
-      console.log(todoResponse)
-      setTodoResponse(todoResponse)
-      // setTask("")
+      const updatedResponse = await TodoEndpoint.findAll()
+      setTodoResponse(updatedResponse)
+      setTask("")
+      const inputTask: HTMLElement = document.getElementById("inputTask")!;
+      inputTask.focus()
     }
   }
 
@@ -37,54 +38,38 @@ export default function TodoView() {
   }
 
   return <div className="p-m">
-    <h2>Hilla Todo Application</h2>
+    <HorizontalLayout theme="margin">
+      <h2>Hilla Todo Application</h2>
+    </HorizontalLayout>
 
-    <div className="flex gap-s">
-      <TextField value={task}
-                 // onChange={e => {
-                 //   setTask(e.target.value)
-                 //   console.log("ONCHANGE", e.target.value)
-                 //   console.log("ONCHANGE", task)
-                 // }}
-                 onKeyDown={(event) => {
-                   if (event.key === 'Enter') {
-                     const value: string = (event.target as HTMLInputElement).value;
-                     setTask(value)
-                     console.log("TASK", task)
-                     console.log("VALUE: ", value)
-                     addTodo()
-                   }
+    <HorizontalLayout theme="margin" className="flex gap-s">
+      <TextField value={task} autoFocus={true} id={"inputTask"}
+                 onChange={e => {
+                   setTask(e.target.value)
                  }}
       />
-      <Button theme="primary" onClick={addTodo}>Add</Button>
-    </div>
-
-    <h5 className="p-m">Uncompleted Tasks</h5>
-    {
-      (todoResponse.unCompleted as [Todo]).map(uncompleted =>
-          <div key={uncompleted.id}>
-            <Checkbox checked={uncompleted?.completed}
-                      onCheckedChanged={e => updateTodo(uncompleted, e.detail.value)}/>
-            <span>{uncompleted.task}</span>
-          </div>)
-    }
-
-    <h5 className="p-m">Completed Tasks</h5>
-    {
-      (todoResponse.completed as [Todo]).map(completed =>
-          <div key={completed.id}>
-            <span>{completed.task}</span>
-          </div>)
-    }
-
-    {/*<div>*/}
-    {/*  <TextField value={message} onKeyDown={(event) => {*/}
-    {/*    if (event.key === 'Enter'){*/}
-    {/*      setMessage((event.target as HTMLInputElement).value)*/}
-    {/*    }*/}
-    {/*  }}></TextField>*/}
-      <h2>Message: {task}</h2>
-    {/*</div>*/}
+      <Button theme="primary" onClick={e => {
+        addTodo()
+      }}>Add</Button>
+    </HorizontalLayout>
+    <VerticalLayout theme="spacing padding margin" className='border border-error-50 rounded-m'>
+      {
+        (todoResponse.unCompleted as [Todo]).map(uncompleted =>
+            <div key={uncompleted.id} >
+              <Checkbox checked={uncompleted?.completed}
+                        onCheckedChanged={e => updateTodo(uncompleted, e.detail.value)}/>
+              <span>{uncompleted.task}</span>
+            </div>)
+      }
+    </VerticalLayout>
+    <VerticalLayout theme="spacing padding margin" className='border border-primary-50 rounded-m'>
+      {
+        (todoResponse.completed as [Todo]).map(completed =>
+            <div key={completed.id}>
+              <span><s>{completed.task}</s></span>
+            </div>)
+      }
+    </VerticalLayout>
 
   </div>;
 }
